e3a9429465420a238436408c116919cc84bc8d99
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2020-07-03 23:32:26 -0300

    fix: merge fails e implementa prop page na forca

--- components/menu.js
+++ components/menu.js
@@ -1,7 +1,7 @@
 const template = document.createElement("template");
 const generateTemplate = (activePage) => {
   template.innerHTML = `
-    <header class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <header class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">Web Games</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -11,11 +11,11 @@ const generateTemplate = (activePage) => {
           <li class="nav-item ${activePage == "home" && "active"}">
             <a class="nav-link" href="/web-games">Home</a>
           </li>
-          <li class="nav-item ${activePage == "forca" && "active"}">
-            <a class="nav-link" href="forca.html">Jogo da Velha</a>
-          </li>
           <li class="nav-item ${activePage == "velha" && "active"}">
-            <a class="nav-link" href="velha.html">Jogo da Forca</a>
+            <a class="nav-link" href="velha.html">Jogo da Velha</a>
+          </li>
+          <li class="nav-item ${activePage == "forca" && "active"}">
+            <a class="nav-link" href="forca.html">Jogo da Forca</a>
           </li>
         </ul>
       </nav>
